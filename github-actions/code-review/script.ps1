0fc21c15fe7d9af63f41a376636e34ca1e52e771
ESSARRAJ Fouad <fouad.systems@gmail.com>
2024-01-20 19:07:22 +0100

    review-pull-request

--- github-actions/code-review/script.ps1
+++ github-actions/code-review/script.ps1
@@ -1,4 +1,18 @@
-$package_name = "Package1"
-Write-Host "Vérification de modification des fichiers de package $package_name "
 
+# Input
+$package_name = "Package1"
+
+# Changed files
+$chanded_files = git diff --name-only HEAD^ HEAD
+
+# Filtrer les fichiers selon une expression regulière
+$filteted_files =  $chanded_files | Where-Object { 
+    $_ -match '^github' -or $_ -match '.php$' 
+}
+
+# Affichage
+foreach ($file in  $filteted_files){
+    Write-Host $file
+}
 # \app\Http\Controllers\Package1
+
