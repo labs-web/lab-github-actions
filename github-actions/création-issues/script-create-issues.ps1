fc760d7801aa1b9a215066cc517355a0000c484c
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-22 22:06:06 +0100

    test 11

--- github-actions/création-issues/script-create-issues.ps1
+++ github-actions/création-issues/script-create-issues.ps1
@@ -40,7 +40,7 @@ function confirm_to_continue($message) {
 $branche_name = ""
 function create_branch_to_do_pull_request {
 
-  $branche_name = "update_backlog_files-" + (Get-Date).ToString('MM-dd-yyyy-hh-mm-ss tt')
+  $branche_name = "update_backlog_files-" + (Get-Date).ToString('MM-dd-yyyy-hh-mm-ss')
   git config --global user.name "ESSARRAJ"
   git config --global user.email "essarraj.fouad@gmail.com"
   git checkout -b $branche_name 
