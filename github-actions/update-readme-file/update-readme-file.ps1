35a62c8898ea6355726db7d786a2dd45a04a20dd
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-22 15:54:06 +0100

    save

--- github-actions/update-readme-file/update-readme-file.ps1
+++ github-actions/update-readme-file/update-readme-file.ps1
@@ -1,7 +1,11 @@
-﻿# Update README file for lab
+﻿# Update README.md file from lab-web.json
+
+
 # Le code source est dévelopé dans /labs-web/scripts/labs/update-readme-file.ps1
  
-# Functions : confirmation
+# L'utilisateur doit exécuter ce script sur la racine du lab
+
+# Functions
 function confirm_to_continue($message) {
     $title    = $message 
     $question = "Are you sure you want to proceed?"
@@ -12,50 +16,39 @@ function confirm_to_continue($message) {
     } 
 }
  
-# L'utilisateur doit exécuter ce script sur le racine du lab
-# confirm_to_continue("Vous devez exécuter ce script sur la racine du lab")
-
-# Paths 
+# Input 
 $depot_path = $(Get-Location).Path
 $readme_path= "$depot_path\README.md"
-$lab_web_data = "$depot_path/lab-web.json"
+$lab_web_data_path = "$depot_path/lab-web.json"
 $backlog_files_path = "$depot_path\backlog"
-# Confirmation
-# confirm_to_continue("Update de fichier $readme_path ")
 
+# Load json_data
+$json_data = Get-Content $lab_web_data_path  | ConvertFrom-Json
 
-# Load JSON
-$json_data = Get-Content $lab_web_data  | ConvertFrom-Json
-
-
-# Création de fichier README
+# 
+# Génération de contenue de fichier README
+#
 
 # Introduction
 $readme_string = "# $($json_data.Introduction.Titre) `n`n"
 $readme_string += "- Référence :  $($json_data.Introduction.Reference) `n`n"
 $readme_string += "$($json_data.Introduction.Description) `n`n"
-
 # Backlog
 $readme_string += "## $($json_data.Backlog.Titre) `n`n"
 $readme_string += "$($json_data.Backlog.Introduction) `n`n"
-
 Get-ChildItem $backlog_files_path |  ForEach-Object {
     $backlog_item_file_name = $_.Name
     $readme_string += "- [$backlog_item_file_name](./Backlog/$backlog_item_file_name) `n"
 } 
-
 ## Livrables 
 $readme_string += "## $($json_data.Livrables.Titre) `n`n"
 $readme_string += "$($json_data.Livrables.Introduction) `n`n"
-
 $json_data.Livrables.Livrables |  ForEach-Object {
     $readme_string += "- $($_.Titre) `n"
     if(-not($_.Description -eq "")){
         $readme_string += "  - $($_.Description) `n"
     }
-    
 } 
-
 ## Références 
 $readme_string += "## $($json_data.References.Titre) `n`n"
 $readme_string += "$($json_data.References.Introduction) `n`n"
@@ -64,8 +57,7 @@ $json_data.References.References |  ForEach-Object {
     $readme_string += "- [$($_.Titre)]($($_.Lien)) `n"
 } 
 
-
-# Check if branch update_readme_file exist
+# Préparation de git for pullrequest
 git config --global user.name "ESSARRAJ"
 git config --global user.email "essarraj.fouad@gmail.com"
 git fetch
@@ -77,11 +69,6 @@ foreach($branch_name in $branch_list ){
         $branch_update_readme_file_exist = $true
     }
 }
-
-# confirm_to_continue("Create pullrequest $readme_path ")
-
-
-# Send PullRequest to develop
 if($branch_update_readme_file_exist){
     Write-Host "git checkout update-readme-file"
     git checkout "update-readme-file"
@@ -90,15 +77,19 @@ if($branch_update_readme_file_exist){
     git checkout -b "update-readme-file" 
     git push --set-upstream origin update-readme-file
 }
-
 git pull
 
 # Enregistrement de fichier README
 Set-Content $readme_path $readme_string
 
-
+# push to  update-readme-file branch
 git add .
 git commit -m "change README.md file to be updated with lab-web.json"
 git push
 
-gh pr create --base develop --title "update readme file" --body "change README.md to lab-web.json"
+# Create pull request if not yet exist
+$pull_request_exist = (gh pr list --json title | ConvertFrom-Json).title -contains "update readme file"
+if(-not($pull_request_exist)){
+    gh pr create --base develop --title "update readme file" --body "change README.md to lab-web.json"
+}
+
