5f68e5489a6399c17d6ceb311cb75fa19df22498
ESSARRAJ <essarraj.fouad@gmail.com>
2024-02-17 09:15:49 +0100

    group

--- github-actions/affichage-message/affichage-message.ps1
+++ github-actions/affichage-message/affichage-message.ps1
@@ -4,5 +4,14 @@ Write-Host $message_erreur
 Write-Host "::error file=app.js,line=1::Missing semicolon"
 Write-Host "::debug::Set the Octocat variable"
 Write-Host "::notice file=app.js,line=1,col=5,endColumn=7::Missing semicolon"
+Write-Host "::warning file=app.js,line=1,col=5,endColumn=7::Missing semicolon"
+
+
+Write-Host "::group:: Validation "
+Write-Host "Message 1"
+Write-Host "Message 2"
+Write-Host "::endgroup::"
+
+
 # Write-error $message_erreur
 exit 1 
