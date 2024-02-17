41704b80acc4753ac28af192ead9ba69f6d4edd7
ESSARRAJ <essarraj.fouad@gmail.com>
2024-02-17 09:08:55 +0100

    save2

---
+++ docs/affichage-message/affichage-message.md
@@ -0,0 +1,4 @@
+# affichage-message
+
+## Références 
+- [Workflow commands for GitHub Actions](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions)
--- github-actions/affichage-message/affichage-message.ps1
+++ github-actions/affichage-message/affichage-message.ps1
@@ -1,5 +1,6 @@
 ﻿
 $message_erreur = "::error::  Message 1"
 Write-Host $message_erreur
-Write-error $message_erreur
+Write-Host "::error file=app.js,line=1::Missing semicolon"
+# Write-error $message_erreur
 exit 1 
