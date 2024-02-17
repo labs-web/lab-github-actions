75a41987ac538d28f635228883d590066feab104
ESSARRAJ <essarraj.fouad@gmail.com>
2024-02-17 09:13:22 +0100

    debug

--- github-actions/affichage-message/affichage-message.ps1
+++ github-actions/affichage-message/affichage-message.ps1
@@ -2,5 +2,6 @@
 $message_erreur = "::error::  Vous n'avez pas le droit de modifier le fichier 1"
 Write-Host $message_erreur
 Write-Host "::error file=app.js,line=1::Missing semicolon"
+Write-Host "::debug::Set the Octocat variable"
 # Write-error $message_erreur
 exit 1 
